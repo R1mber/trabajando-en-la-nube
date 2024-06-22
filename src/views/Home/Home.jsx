8c494330f2139de9f2a48f6288163f8898a0be34
Rimber <rimber.re95@gmail.com>
2024-06-22 00:11:40 -0400

    tre

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,4 +1,4 @@
-import { collection, doc, getDoc, getDocs } from 'firebase/firestore'
+import { collection, getDocs } from 'firebase/firestore'
 import { db } from './../../config/firebase'
 import { useState, useEffect } from 'react'
 
